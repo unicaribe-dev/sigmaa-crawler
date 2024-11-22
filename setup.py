6f6ae03d33c30fa1b2a2a4f210c11d347ac9849f
Kenneth Díaz <kennethdiazgonzalez@hotmail.com>
2024-11-21 19:50:40 -0500

    Updated package README import

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+
+def read_long_description() -> str:
+    with open("README.md", "r") as file:
+        return file.read()
+
 setup(
-    name="SigmmaCrawler",
-    version="0.0.1.dev",
+    name="sigmaa-crawler",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "requests",
@@ -13,11 +18,12 @@ setup(
     description="Este proyecto permite obtener datos del SIGMAA, el sistema educativo de la Universidad del Caribe. "
                 "Facilita la obtención de información del estudiante, calificaciones, cárdex, oferta académica, "
                 "información de pagos de estudiantes, entre otros datos, a través de un SDK de Python.",
-    long_description=open("README.md").read(),
+    license="MIT",
+    long_description=read_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/unicaribe-dev/sigmaa-crawler",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
